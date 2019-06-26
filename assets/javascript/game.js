7d5e5f1858fead3b8920749b2c0b04d35e42b5b7
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-06-26 13:17:28 -0400

    created a title in the html file and created a very vauge pseudocode in the javascript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// Define Variables
+
+// Define Functions
+
+// Main Section of Game
+
+    //Computer randomly picks a type of rabbit from the array
+    //Computer displays the word in this format _ _ _ _ _ _ _
+    //User enters a letter
+    //Computer stores the letter
+    //Computer checkes to see if that letter has already been used
+        //If letter has been used, does NOT count the tries
+        //If letter has NOT been used, computer determines if it's in the word or not
+    //When user wins, increase the win count by one
+    //When loser runs out of try counts, resets the try counter and displays the word
+    //Bonus? Change images based on what the word is
